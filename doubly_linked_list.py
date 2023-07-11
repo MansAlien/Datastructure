86297f6b714586a8fcb462d9e6ec338915961f92
MansAlien <mans012033945421@gmail.com>
2023-07-11 13:37:40 +0300

    create add_before method; to add a node before a given element

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -53,6 +53,24 @@ class doubly_linked_list:
 					break
 				elif current.data != element:
 					current = current.next
+
+	def add_befor(self, data, element):
+		new_node = Node(data)
+		current = self.head
+		while current:
+			if current is None:
+				print("Linked list is empty!")
+			else:
+				if current.next is None:
+					print("The element is not found")
+				if current.data == element:
+					new_node.next = current
+					new_node.pre = current.pre
+					current.pre.next = new_node
+					current.pre = new_node
+					break
+				elif current.data != element:
+					current = current.next
 				
 
 
@@ -72,5 +90,6 @@ l1 = doubly_linked_list()
 l1.add_empty(10)
 l1.add_begin(0)
 l1.add_end(20)
-l1.add_after("x", 43)
+l1.add_after("x", 10)
+l1.add_befor("y", 10)
 l1.display()
